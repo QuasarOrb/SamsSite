55a526f6e44d3f8433ea10cf805357cd6adf6f99
QuasarOrb <105732138+QuasarOrb@users.noreply.github.com>
2023-10-01 12:26:13 -0600

    Update choose.js

--- choose.js
+++ choose.js
@@ -4,15 +4,19 @@ let question = ["Toy (plural)","Musical Instrument (plural)", "noun","food (plur
 let answers = []
 let story = ["If I was Principal at my school I would put ", " and "," in every "," and have the cafeteria serve "," and ", " for lunch. We would have "," and Tell everyday, where students can bring ", " and "," to share in class. Students would give teachers homework, like a "," page book reports about ", ", and "," math problems. Recess would last ", " hours, and instead of buses I would have ", " and ", " take the kids to and from school."]
 function start() {
-    answers = []
+    if(answers.length == 14) {
+        answers = []
+        answers.length = 0
+    }
     document.getElementById("story").innerHTML = ""
-    for (let i =0;i <= 13; i++){
+    for (let i = answers.length;i <= 13; i++){
         answers.push(prompt(question[i], ""))
         if(answers[i] == null || answers[i] == ""){
+            answers.pop()
             return
         }
-    }
     
+    }
     for(i = 0;i<=story.length - 2;i++){
         document.getElementById("story").innerHTML += story[i] + answers[i]
         
